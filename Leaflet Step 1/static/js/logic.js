070ef42bb6baa27b9ab17d5a53e1fdd378eeb5dd
crismariano <cris.mariano6@gmail.com>
2020-11-29 09:53:18 -0500

    commit 6

--- Leaflet Step 1/static/js/logic.js
+++ Leaflet Step 1/static/js/logic.js
@@ -48,19 +48,18 @@ d3.json(queryUrl, function(data) {
     else {
       color = "#FF0000";
     }
-    // set circle markers for magnitude scales
+    // Set circle markers for magnitude scales
     L.circle(latLng, {
       fillOpacity: 0.75,
       color: "none",
       fillColor: color,
-      radius: magnitude * 40000
-    }).bindPopup("<h1>" + title + "</h1>").addTo(myMap);
-
+      radius: magnitude * 30000
+    }).bindPopup("<h3>Earthquake Info</h3>" + "<hr>" + "<h3>" + title + "</h3>").addTo(myMap);
   }
 });
 
 // Define streetmap
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var light = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -69,7 +68,10 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Create Legend
+
+
+
+
 
 
 
